ded815d5863d0b5e89d430ea5707a86af7845a20
Alexander L. Kyunov <san@modem.by>
2018-06-08 14:46:32 +0300

    Added map and footer on main page. Added catolog page. Working on styles.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/catalog.js
+++ routes/catalog.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-const viewName = 'index';
+const viewName = 'catalog';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render(viewName, { 
-    title: 'Главная',
-    activeView: viewName
-  });
+      title: 'Каталог', 
+      activeView: viewName
+    });
 });
 
 module.exports = router;
