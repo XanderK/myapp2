8c1d1c6c78bb938942000f301a42dfa4dd47b2f0
Alexander Kyunov <kyunov@gmail.com>
2019-02-18 08:08:35 +0300

    Working on CRUD of images

--- api/models/Product.js
+++ api/models/Product.js
@@ -14,6 +14,7 @@ const productSchema = new mongoose.Schema({
   owner: { type: userSchema, required: true, index: true },
   responsible: String,
   images: [String],
+  //storedImages: [{id: String, src: String, value: String}],
   mainImageIndex: Number
 });
 
